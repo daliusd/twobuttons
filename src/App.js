1c3b5e7f4cf0864fdf73f90adf710ddfe96fa286
Dalius Dobravolskas <daliusd@wix.com>
2021-05-20 17:22:25 +0300

    useEventCallback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useLayoutEffect, useCallback } from 'react';
 import './App.css';
+
+// useEventCallback
+//
+// Source: https://github.com/facebook/react/issues/14099
+// As well read here: https://reactjs.org/docs/hooks-faq.html#how-to-avoid-passing-callbacks-down
+
+function useEventCallback(fn) {
+  let ref = useRef();
+  useLayoutEffect(() => {
+    ref.current = fn;
+  });
+  return useCallback((...args) => (0, ref.current)(...args), []);
+}
+
+// App
 
 function B2({ log }) {
   const [clicks, setClicks] = useState(0);
+  const logCallback = useEventCallback(log);
 
   useEffect(() => {
-    log(`B2 value: ${clicks}`)
-  }, [clicks, log]);
+    logCallback(`B2 value: ${clicks}`)
+  }, [clicks, logCallback]);
 
   const handleClick = () => {
     setClicks(clicks + 1);
